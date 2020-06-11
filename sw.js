c984942d497ad83da0e587ce50c968ce29bd21ca
Adam El-Masri <adam.el.masri@blockchainmarkets.com>
2020-06-10 22:00:52 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c3c84298259862db28f0.js"
+    "url": "webpack-runtime-7175e13e216be9fce598.js"
   },
   {
     "url": "framework-d54fcf3f0204b459cf2d.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "93e374d5d44d9106dda5f2fa2e795882"
+    "revision": "faf7aebb5b862713313dd0455c8ebc41"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c25716df4fbb2532700a.js"
