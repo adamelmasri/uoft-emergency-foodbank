65840475e65df68f19b82f20e760b56acd873501
Adam El-Masri <adam.el.masri@blockchainmarkets.com>
2020-12-03 16:10:35 -0500

    Update donation link

--- src/pages/index.js
+++ src/pages/index.js
@@ -90,7 +90,7 @@ const IndexPage = () => (
             <ul className="actions fit">
                 <li><a target="_blank" rel="noreferrer nofollow" href="https://docs.google.com/forms/d/1gKakfbU5J2rJsxSrt80G6jPQA6c9nWuMa1l1AZHCDXc/edit?ts=5e8f99fe" className="icon fa-user-plus button primary fit">Register for a box</a></li>
                 <li><a rel="noreferrer nofollow" href="https://fundraise.ewb.ca/https-fundraise-ewb_emergencyfoodbank" className="button primary fit icon fa-dollar">Donate now</a></li>
-                <li><a rel="noreferrer nofollow" href="/additional-resources" className="button fit icon fa-search">See additional resources</a></li>
+                <li><a target="_blank" rel="noreferrer nofollow" href="/additional-resources" className="button fit icon fa-search">See additional resources</a></li>
               </ul>
           </div>
         </section>
@@ -160,7 +160,7 @@ const IndexPage = () => (
               if you'd like to support it through a partnership or sponsorship. We also accept 
 
               <strong>
-              <a href="https://fundraise.ewb.ca/https-fundraise-ewb_emergencyfoodbank"> 
+              <a target="_blank" href="https://fundraise.ewb.ca/https-fundraise-ewb_emergencyfoodbank"> 
               &nbsp;financial donations. &nbsp;
               </a> 
               </strong>
